2f765d9b7b9c9179932ceb4e6a6e3cd9d052a9a3
Christopher Lee <hanernlee@gmail.com>
2017-09-29 02:01:13 +1000

    Move Robot in correct directions

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,13 +1,38 @@
 module ToyRobot
   class Robot
-    attr_reader :position
+    attr_reader :east, :north
 
-    def initialize(position = 0)
-      @position = position
+    def initialize(east = 0, north = 0, direction = "NORTH")
+      @east = east
+      @north = north
+      @direction = direction
     end
 
+    #using metaprogramming here
     def move
-      @position += 1
+      # case @direction
+      # when "NORTH" then move_north
+      # when "SOUTH" then move_south
+      # when "EAST" then move_east
+      # when "WEST" then move_west
+      # end
+      send("move_#{@direction.downcase}")
     end
+
+    def move_east
+      @east += 1
+    end
+
+    def move_west
+      @east -= 1
+    end
+
+    def move_north
+      @north += 1
+    end
+
+    def move_south
+      @north -= 1
+    end    
   end
 end
